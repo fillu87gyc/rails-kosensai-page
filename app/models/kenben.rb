df83d9e18a54b72fa2efa546f8ecdc0d5f77e80f
fillu87gyc <s1500604@sendai.kosen-ac.jp>
2019-07-01 00:31:08 +0900

    :heavy_plus_sign: バリデーション追加

--- app/models/kenben.rb
+++ app/models/kenben.rb
@@ -1,3 +1,4 @@
 class Kenben < ApplicationRecord
   belongs_to :team
+  validates :name, presence: true
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -2,4 +2,5 @@ class Team < ApplicationRecord
   belongs_to :user
   has_many :kenbens
   accepts_nested_attributes_for :kenbens
+  validates :team_name, presence: true
 end
