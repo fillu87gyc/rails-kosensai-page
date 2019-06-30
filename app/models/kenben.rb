c484a1dcca44efaabf068588e493778dbd04bbfa
fillu87gyc <s1500604@sendai.kosen-ac.jp>
2019-07-01 00:47:07 +0900

    :heavy_plus_sign: update

--- app/models/kenben.rb
+++ app/models/kenben.rb
@@ -1,4 +1,8 @@
 class Kenben < ApplicationRecord
   belongs_to :team
   validates :name, presence: true
+  validates :age, presence: true
+  validates :sex, presence: true
+  validates :phonetic, presence: true
+  validates :class_name, presence: true
 end
